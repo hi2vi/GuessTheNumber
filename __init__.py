0c5bc6d41bd896c6aceaa1c94e6070387a3a4ff4
hi2vi <115847899+hi2vi@users.noreply.github.com>
2022-10-27 10:39:08 -0400

    Update __init__.py(2)

--- __init__.py
+++ __init__.py
@@ -1,1 +1,29 @@
-import guess_the_number
+class Game:
+  random = __import__('random')
+  def __init__(self,guesses=5,number=None,a=None,b=None):
+    self.guesses = gusses
+    if number is None:
+      self.number = random.randint(a,b)
+    else:
+      self.number = number
+    
+    self.start_game()
+   
+   def start_game(self):
+    for i in range(self.guesses):
+      guess = int(input("Enter a guess: "))
+      
+      if guess > self.number:
+        print("Your number is too high.")
+      if guess == self.number:
+        print("You won in just {i} guesses!")
+        break
+      if guess < self.number:
+        print("You number is too low.")
+        
+   def reset(self,guesses=5,number=None,a=None,b=None):
+    self.guesses = gusses
+    if number is None:
+      self.number = random.randint(a,b)
+    else:
+      self.number = number
