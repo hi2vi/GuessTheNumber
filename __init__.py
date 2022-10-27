f6553ed8549830b82f33af063ce0e321e7950d2e
hi2vi <115847899+hi2vi@users.noreply.github.com>
2022-10-27 10:42:09 -0400

    Update __init__.py(3)

--- __init__.py
+++ __init__.py
@@ -10,16 +10,16 @@ class Game:
     self.start_game()
    
    def start_game(self):
-    for i in range(self.guesses):
-      guess = int(input("Enter a guess: "))
-      
-      if guess > self.number:
-        print("Your number is too high.")
-      if guess == self.number:
-        print("You won in just {i} guesses!")
-        break
-      if guess < self.number:
-        print("You number is too low.")
+      for i in range(self.guesses):
+        guess = int(input("Enter a guess: "))
+
+        if guess > self.number:
+          print("Your number is too high.")
+        if guess == self.number:
+          print("You won in just {i} guesses!")
+          break
+        if guess < self.number:
+          print("You number is too low.")
         
    def reset(self,guesses=5,number=None,a=None,b=None):
     self.guesses = gusses
